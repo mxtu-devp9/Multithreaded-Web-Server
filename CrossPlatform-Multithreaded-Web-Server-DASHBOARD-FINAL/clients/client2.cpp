543ea7f7212aab1ae5e7f25c8a7eb2c7e35a3a83
Miten Mistry <165764310+mxtu-devp9@users.noreply.github.com>
2026-07-02 11:39:32 +0200

    Delete CrossPlatform-Multithreaded-Web-Server-DASHBOARD-FINAL directory

--- CrossPlatform-Multithreaded-Web-Server-DASHBOARD-FINAL/clients/client2.cpp
+++
@@ -1,6 +0,0 @@
-#include "client_common.h"
-int main(int argc, char* argv[]) {
-    std::string serverIp = argc > 1 ? argv[1] : "127.0.0.1";
-    sendClientRequest(serverIp, "/client-status", "Avi", "Normal");
-    return 0;
-}
